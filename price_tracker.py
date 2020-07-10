de4536589ba962b82f154a877df3456e2396fac2
mezzofortissimo <noel.dolores@gmail.com>
2020-07-09 19:07:56 -0700

    test

--- price_tracker.py
+++ price_tracker.py
@@ -6,19 +6,34 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+#import boto
+#from boto.s3.connection import S3Connection
+#import psycopg2
 
-reddit = praw.Reddit('bot1')
+#Connect to database
+#DATABASE_URL = os.environ['DATABASE_URL']
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+#reddit = praw.Reddit('BuildAPCSalesTracker')
+
+#reddit.login(os.environ['REDDIT_USER'], os.environ['REDDIT_PASS'])
+
+reddit = praw.Reddit(client_id=os.environ['CLIENT_ID'],
+                     client_secret=os.environ['CLIENT_SECRET'],
+                     user_agent=os.environ['USER_AGENT'],
+                     username=os.environ['REDDIT_USER'],
+                     password=os.environ['REDDIT_PASS'])
 subreddit = reddit.subreddit("buildapcsales")
 
 # Have we run this code before? If not, create an empty list
-if not os.path.isfile("Log.txt"):
-	log = []
-else:
+#if not os.path.isfile("Log.txt"):
+#	log = []
+#else:
 	# If we have run the code before, load the list of posts we have replied to
-	with open("Log.txt", "r") as f:
-		log = f.read()
-		log = log.split("\n")
-		log = list(filter(None, log))
+#	with open("Log.txt", "r") as f:
+#		log = f.read()
+#		log = log.split("\n")
+#		log = list(filter(None, log))
 
 for submission in subreddit.new(limit=10):
 	print("Title: ", submission.title)
@@ -35,19 +50,21 @@ for submission in subreddit.new(limit=10):
 
 		if pc_part in post_title:
 			 # If we haven't replied to this post before
-			if submission.id not in log:
+			#if submission.id not in log:
+			if not submission.saved
 				# Store the current id into our list
-				log.append(submission.id)
+				#log.append(submission.id)
+				submission.save()
 				# Write our updated list back to the file
-				with open("Log.txt", "w") as f:
-					for post_id in log:
-						f.write(post_id + "\n")
+				#with open("Log.txt", "w") as f:
+				#	for post_id in log:
+				#		f.write(post_id + "\n")
 
 				port = 465  # For SSL
 				smtp_server = "smtp.gmail.com"
 
-				sender_email = "buildapcsalespricetracker@gmail.com"
-				password = "PythonBot#%"
+				sender_email = os.environ['EMAIL_ADDRESS']
+				password = os.environ['EMAIL_PASSWORD']
 
 				message = MIMEMultipart("alternative")
 				message["Subject"] = "Sale Alert"
